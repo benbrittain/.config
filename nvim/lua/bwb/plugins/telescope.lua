1d6511597f31f766b15c16c44191e9a11b6ad76e
Benjamin Brittain <ben@brittain.org>
2024-03-30 11:57:26 -0400

    Telescope should use .gitignore even if no .git is present

--- nvim/lua/bwb/plugins/telescope.lua
+++ nvim/lua/bwb/plugins/telescope.lua
@@ -9,6 +9,7 @@ return {
   config = function()
     local telescope = require("telescope")
     local actions = require("telescope.actions")
+    local builtin = require("telescope.builtin")
 
     telescope.setup({
       defaults = {
@@ -26,9 +27,15 @@ return {
     telescope.load_extension("fzf")
 
     -- set keymaps
-    local keymap = vim.keymap -- for conciseness
+    local keymap = vim.keymap
 
-    keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+    keymap.set('n', '<leader>ff', function()
+      builtin.find_files({
+        hidden = true,
+        file_ignore_patterns = { ".git", '.jj' },
+        find_command = { "rg", "--files", "--no-require-git" },
+      })
+    end, { desc = "Fuzzy find files in cwd" })
     keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
     keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
     keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
