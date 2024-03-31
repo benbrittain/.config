26809f58bab773cb25affab9967d50aecf984481
Benjamin Brittain <ben@brittain.org>
2024-03-31 17:41:51 -0400

    Make todo-comments in an everforest pallete

--- nvim/lua/bwb/plugins/todo-comments.lua
+++ nvim/lua/bwb/plugins/todo-comments.lua
@@ -5,12 +5,12 @@ return {
     local todo= require('todo-comments')
     todo.setup({
         colors = {
-        error = { "DiagnosticError", "ErrorMsg", "#DC2626" },
-        warning = { "DiagnosticWarn", "WarningMsg", "#FBBF24" },
-        info = { "DiagnosticInfo", "#2563EB" },
-        hint = { "DiagnosticHint", "#10B981" },
-        default = { "Identifier", "#7C3AED" },
-        test = { "Identifier", "#FF00FF" }
+        error = { "DiagnosticError", "ErrorMsg", "#E67E80" },
+        warning = { "DiagnosticWarn", "WarningMsg", "#E69975" },
+        info = { "DiagnosticInfo", "#DBBC75" },
+        hint = { "DiagnosticHint", "#7FBBB3" },
+        default = { "Identifier", "#A7C080" },
+        test = { "Identifier", "#D699B6" }
       },
     --     strategy = {},
     --     query = {},
